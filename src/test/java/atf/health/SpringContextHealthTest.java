b7fedf29a2a056f3b6d10ef6af158d4b2fff30f7
Sergiu Ababii <Vehkiya@users.noreply.github.com>
2021-02-09 11:01:09 +0200

    Created custom CircleCI workflows for build/regression (#7)

--- src/test/java/atf/health/SpringContextHealthTest.java
+++ src/test/java/atf/health/SpringContextHealthTest.java
@@ -3,10 +3,12 @@ package atf.health;
 import atf.configuration.TestConfiguration;
 import atf.properties.EnvironmentProperties;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Tag("health")
 @SpringBootTest(classes = TestConfiguration.class)
 public class SpringContextHealthTest {
 
